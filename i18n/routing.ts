6e506c2488c1c7f7a2750c647641ef0b26661e2a
Elemar Termulo <jetermulo@gmail.com>
2026-04-09 12:40:36 +0800

    fix: ensure localePrefix is set to 'never' in routing configuration

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,6 +4,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en"],
   defaultLocale: "en",
+  localePrefix: "never",
 });
 
 export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
